784151512aec04d4e3e8aa4f610fd075314948fc
Flolol0o <116642272+Flolol0o@users.noreply.github.com>
2025-11-09 01:50:21 +0100

    round missing play

--- Domain/src/model/Round.ts
+++ Domain/src/model/Round.ts
@@ -1,9 +1,10 @@
 import * as _ from "lodash";
 
 import * as card from "./Card"
+import * as CardFactory from "./CardFactory"
 import * as deck from "./Deck";
 import * as player from "./Player";
-import { Hand } from "./Hand";
+import * as hand from "./Hand";
 
 export enum Direction {
   Clockwise = "clockwise",
@@ -13,9 +14,9 @@ export enum Direction {
 type Deck = deck.Deck;
 type Player = player.Player;
 type Card = card.Card;
-type CardType = card.Type;
+type Hand = hand.Hand;
 
-type Round =  {
+export type Round =  {
   readonly players: Player[];
   readonly currentDirection: Direction;
   readonly currentPlayer: number;
@@ -26,7 +27,7 @@ type Round =  {
   readonly winner?: Player;
 };
 
-function createNewRound(players: Player[], dealer: number): Round {
+export function initializeRound(players: Player[], dealer: number): Round {
   let drawPile = deck.createNewDrawPile();
 
   let playersWithCards = _.cloneDeep(players);
@@ -78,8 +79,9 @@ function getPlayerHand(player: player.PlayerNames, oldRound: Round): Hand | unde
   return getSpecificPlayer(oldRound,player)?.hand
 }
 
-function updateCurrentDirection(direction: Direction, oldRound: Round): Round {
-  return {...oldRound, currentDirection: direction}
+function updateCurrentDirection(oldRound: Round): Round {
+  const newDirection = oldRound.currentDirection === Direction.Clockwise ? Direction.CounterClockwise : Direction.Clockwise;
+  return {...oldRound, currentDirection: newDirection}
 }
 
 function getPlayersCard(player: player.PlayerNames, card: number, oldRound: Round): Card | undefined {
@@ -155,7 +157,7 @@ function canPlay(cardId: number, oldRound: Round): boolean {
         return true;
 
       case card.Type.Numbered:
-        if (oldRound.topCard.CurdNumber === card.CurdNumber || oldRound.topCard.Color === card.Color){
+        if (oldRound.topCard.CardNumber === card.CardNumber || oldRound.topCard.Color === card.Color){
             return true;
         }
         return false;
@@ -167,6 +169,130 @@ function canPlay(cardId: number, oldRound: Round): boolean {
         return false
     }
 }
+
+function sayUno(playerId: player.PlayerNames, oldRound: Round): Round {
+    const specificPlayer = getSpecificPlayer(playerId, oldRound)
+    const playersHand = getPlayerHand(playerId, oldRound)
+    let updated = undefined
+    let message = ""
+    if (playersHand.length === 2) {
+      if (canPlay(0, oldRound) || canPlay(1, oldRound)) {
+        player.setUno(true, specificPlayer)
+        message = specificPlayer.name + " called UNO!"
+      }
+      else {
+        updated = draw(4,playerId,oldRound)
+        message = specificPlayer.name + " called UNO and failed!"
+      }
+      if (!updated) {
+        updated = oldRound
+      }
+      return {...updated, statusMessage: message}
+    }
+    if (playersHand.length != 1) {
+        updated = draw(4,playerId,oldRound)
+        message = specificPlayer.name + " called UNO and failed!"
+      return {...updated, statusMessage: message}
+    }
+    player.setUno(true, specificPlayer)
+    message = specificPlayer.name + " called UNO!"
+    return {...oldRound, statusMessage: message}
+
+}
+
+function getNextPlayer(oldRound: Round): player.PlayerNames {
+    let index = 0;
+    if (oldRound.currentDirection === Direction.Clockwise)
+      index = (oldRound.players.findIndex((p) => p.playerName === oldRound.currentPlayer) + 1) % oldRound.players.length
+    else
+      index = (oldRound.players.findIndex((p) => p.playerName === oldRound.currentPlayer) - 1 + oldRound.players.length) % oldRound.players.length
+    return oldRound.players[index].playerName
+}
+
+function getPreviousPlayer(oldRound: Round): player.PlayerNames {
+    let index = 0;
+    if (oldRound.currentDirection === Direction.Clockwise)
+      index = (oldRound.players.findIndex((p) => p.playerName === oldRound.currentPlayer) - 1  + oldRound.players.length) % oldRound.players.length
+    else
+      index = (oldRound.players.findIndex((p) => p.playerName === oldRound.currentPlayer) + 1) % oldRound.players.length
+    return oldRound.players[index].playerName
+}
+
+  function couldPlayInsteadofDrawFour(oldRound: Round): boolean {
+    const hand = getPlayerHand(getPreviousPlayer(oldRound), oldRound).cards
+
+    for (let i = 0; i < hand.length; i++) {
+      switch (hand[i].Type) {
+        case card.Type.Reverse:
+        case card.Type.Draw:
+        case card.Type.Skip:
+          if (oldRound.topCard.Type === hand[i].Type || oldRound.topCard.Color === hand[i].Color) {
+            return true;
+          }
+          break;
+        case card.Type.Numbered:
+          if (oldRound.topCard.CardNumber === hand[i].CardNumber || oldRound.topCard.Color === hand[i].Color) {
+            return true;
+          }
+        case card.Type.Wild:
+        case card.Type.WildDrawFour:
+        case card.Type.Dummy:
+        case card.Type.DummyDraw4:
+          break;
+      }
+    }
+    return false;
+  }
+
+  function challengeWildDrawFour(isChallenged: boolean, oldRound: Round): [boolean, Round] {
+    let newRound = undefined
+    let message = ""
+
+    if (!isChallenged) {
+      newRound = draw(4, oldRound.currentPlayer, oldRound)
+      message = getSpecificPlayer(newRound.currentPlayer, newRound).name + " did not challenge"
+      return [false, {...newRound, statusMessage: message, currentPlayer: getNextPlayer(newRound)}];
+    }
+
+    if (!couldPlayInsteadofDrawFour(oldRound)){
+      newRound = draw(4, getPreviousPlayer(oldRound), oldRound)
+      message = getSpecificPlayer(newRound.currentPlayer, newRound).name + " challenged successfully"
+      return [true, {...newRound, statusMessage: message}];
+    }
+    newRound = draw(6, oldRound.currentPlayer, oldRound)
+    message = getSpecificPlayer(newRound.currentPlayer, newRound).name + " challenged but failed"
+    return [false, {...newRound, statusMessage: message, currentPlayer: getNextPlayer(newRound)}];
+  }
+
+  export function handleStartRound(oldRound: Round): Round {
+    const currentCard = oldRound.topCard
+
+    switch (currentCard.Type) {
+      case card.Type.Skip:
+        return {...oldRound, currentPlayer: getNextPlayer(oldRound)}
+      case card.Type.Reverse:
+        const reverseRound = updateCurrentDirection(oldRound)
+        return {...reverseRound, currentPlayer: getNextPlayer(reverseRound)} //we dont need to cal next player 2 since we are passing updated round
+      case card.Type.Draw:
+        const drawRound = draw(2, oldRound.currentPlayer, oldRound)
+        return {...drawRound, currentPlayer: getNextPlayer(drawRound)}
+      case card.Type.WildDrawFour:
+        const wildDrawPile = _.flow([deck.addCard, deck.shaffle])
+        const tempDrawPile = wildDrawPile(currentCard, oldRound.drawPile)
+        const [newTopCard, newDrawPile] = deck.deal(tempDrawPile)
+        const newDiscardPile = deck.addCard(newTopCard, oldRound.discardPile)
+        
+        const updatedRound = {...oldRound, topCard:newTopCard, discardPile: newDiscardPile, drawPile: newDrawPile}
+        return handleStartRound(updatedRound)
+      default:
+        return oldRound;
+    }
+  }
+
+  function setWildCard(color: card.Color, oldRound: Round): Round {
+    const newDiscardPile = deck.addCard(CardFactory.CreateDummyCard(color),oldRound.discardPile)
+    return {...oldRound, discardPile: newDiscardPile}
+  } 
 
 //Helper Functions
 //Draw
