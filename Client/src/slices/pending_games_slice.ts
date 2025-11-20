95f3ef162984c1dfd641731b12f681b6d573815b
KaBence <kabaly.bence@gmail.com>
2025-11-20 17:56:14 +0100

    pending slice fix

--- Client/src/slices/pending_games_slice.ts
+++ Client/src/slices/pending_games_slice.ts
@@ -1,19 +1,19 @@
-import type { Game } from "domain/src/model/Game";
+import type { GameSpecs } from "../model/game";
 import { createSlice, type PayloadAction } from "@reduxjs/toolkit"
 import * as _ from 'lodash/fp'
 
-export type PendingGamesState = Readonly<Game[]>
+export type PendingGamesState = Readonly<GameSpecs[]>
 
-export const findPendingGame = (id: string, state: PendingGamesState): Game | undefined => _.find(_.matches({id}), state)
+export const findPendingGame = (id: string, state: PendingGamesState): GameSpecs | undefined => _.find(_.matches({id}), state)
 
 const init_state: PendingGamesState = []
 
 const pending_games_reducers = {
-  reset(_state: PendingGamesState, action: PayloadAction<Game[]>): PendingGamesState{
+  reset(_state: PendingGamesState, action: PayloadAction<GameSpecs[]>): PendingGamesState{
     return action.payload
   },
 
-  upsert(state: PendingGamesState, action: PayloadAction<Game>): PendingGamesState {
+  upsert(state: PendingGamesState, action: PayloadAction<GameSpecs>): PendingGamesState {
     const index = _.findIndex(_.matches({id: action.payload.id}), state)
     if (index === -1)
       return _.concat(action.payload, state)
