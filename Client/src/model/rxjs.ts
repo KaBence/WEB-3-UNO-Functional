555f3fb1ca4e78498a98302098195d14a08a174e
KaBence <114525900+KaBence@users.noreply.github.com>
2025-11-20 18:29:57 +0100

    Slices api rxjs (#9)
    * slices and store
    
    * Specs for client
    
    * slices update
    
    * api and rxjs
    
    * thunks
    
    * forgot one rename
    
    * GameSpecs fix
    
    * pending slice fix

--- Client/src/model/rxjs.ts
+++ Client/src/model/rxjs.ts
@@ -1,13 +1,13 @@
 import type { ApolloClient, DocumentNode } from "@apollo/client";
 import { Subject } from "rxjs";
 
-export async function subscriptionsRxJS<T, E, Key extends string, EventType extends Record<Key, E>>(apolloClient: ApolloClient, subscriptionQuery: DocumentNode, extractor: (evt: EventType) => T): Promise<Subject<T>> {
-  const subject = new Subject<T>();
-  const apolloObservable = apolloClient.subscribe<EventType>({ query: subscriptionQuery })
+export async function subscriptionsRxJS<GamesFeed>(apolloClient: ApolloClient, subscriptionQuery: DocumentNode): Promise<Subject<GamesFeed>> {
+  const subject = new Subject<GamesFeed>();
+  const apolloObservable = apolloClient.subscribe<GamesFeed>({ query: subscriptionQuery })
   apolloObservable.subscribe({
     next({data}) {
       if (data) {
-        subject.next(extractor(data))
+        subject.next(data)
       }
     },
     error(err) {
--- Client/src/slices/active_games_slice.ts
+++ Client/src/slices/active_games_slice.ts
@@ -1,24 +1,32 @@
-import type { Game } from "./../../../Domain/src/model/Game"
 import { createSlice, type PayloadAction } from "@reduxjs/toolkit"
 import * as _ from 'lodash/fp'
+import type { GameSpecs } from "../model/game"
 
-const init_state: Readonly<Game[]> = []
+export type ActiveGamesState = Readonly<GameSpecs[]>
+
+export const findActiveGame = (id: string, state: ActiveGamesState): GameSpecs | undefined => _.find(_.matches({id}), state)
+
+const init_state: ActiveGamesState = []
 
 const active_games_reducers = {
-    reset(_state: Readonly<Game[]>, action: PayloadAction<Game[]>): Readonly<Game[]> {
+    reset(_state: ActiveGamesState, action: PayloadAction<GameSpecs[]>): ActiveGamesState {
         return action.payload
     },
 
-    upsert(state: Readonly<Game[]>, action: PayloadAction<Game>): Readonly<Game[]> {
+    upsert(state: ActiveGamesState, action: PayloadAction<GameSpecs>): ActiveGamesState {
         const index = _.findIndex(_.matches({ id: action.payload.id }), state)
         if (index === -1)
             return _.concat(state, action.payload)
         return _.set(index, action.payload, state)
+    },
+
+    remove(state: ActiveGamesState, action: PayloadAction<{id:number}>): ActiveGamesState {
+    return _.remove(_.matches({id: action.payload.id}), state)
     }
 }
 
 export const active_games_slice = createSlice({
-    name: 'Active games slice',
+    name: 'Active games',
     initialState: init_state,
     reducers: active_games_reducers
 })
