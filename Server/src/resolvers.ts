93d6ff9e92a66394bda4b35e583d78d4eb67dc52
KaBence <kabaly.bence@gmail.com>
2025-11-22 21:07:27 +0100

    Resolver

--- Server/src/resolvers.ts
+++ Server/src/resolvers.ts
@@ -1,79 +1,74 @@
 import { PubSub } from "graphql-subscriptions";
-// import { GameAPI } from "./api";
+import { GameAPI } from "./api";
 
-export const create_Resolvers = () => {
+export const create_Resolvers = (pubsub: PubSub, api: GameAPI) => {
   return {
     Query: {
       activeGames: async () => {
-        // return await this.api.getActiveGames();
+        return await api.getActiveGames();
       },
       pendingGames: async () => {
-        // return await this.api.getPendingGames();
+        return await api.getPendingGames();
       },
     },
     Mutation: {
       createGame: async () => {
-        // return await this.api.createGame();
+        return await api.createGame();
       },
       addPlayer: async (
         _: any,
         { gameId, playerName }: { gameId: number; playerName: string }
       ) => {
-        // return this.api.addPlayer(gameId, playerName);
+        return await api.addPlayer(gameId, playerName);
       },
       removePlayer: async (
         _: any,
         { gameId, playerId }: { gameId: number; playerId: number }
       ) => {
-        // return this.api.removePlayer(gameId, playerId);
+        return await api.removePlayer(gameId, playerId);
       },
       startRound: async (_: any, { gameId }: { gameId: string }) => {
-        // return await this.api.startRound(parseInt(gameId));
+        return await api.startRound(parseInt(gameId));
       },
       playCard: async (
         _: any,
         { gameId, cardId, chosenColor }: { gameId: number; cardId: number; chosenColor: string }
       ) => {
-        // return this.api.playCard(gameId, cardId, chosenColor);
+        return await api.playCard(gameId, cardId, chosenColor);
       },
       drawCard: async (_: any, { gameId }: { gameId: string }) => {
-        // return await this.api.drawCard(parseInt(gameId));
+        return await api.drawCard(parseInt(gameId));
       },
       unoCall: async (_: any, { gameId, playerId }: { gameId: number; playerId: number }) => {
-        // return await this.api.unoCall(gameId, playerId);
+        return await api.unoCall(gameId, playerId);
       },
       accuseUno: async (
         _: any,
         { gameId, accuser, accused }: { gameId: number; accuser: number; accused: number }
       ) => {
-        // return this.api.accuseUno(gameId, accuser, accused);
+        return await api.accuseUno(gameId, accuser, accused);
       },
       challengeDraw4: async (_: any, { gameId, response }: { gameId: number; response: boolean }) => {
-        // return this.api.challengeDraw4(gameId, response);
+        return await api.challengeDraw4(gameId, response);
       },
       canPlay: async (_: any, { gameId, cardId }: { gameId: number, cardId: number }) => {
-        // return this.api.canPlay(gameId,cardId)
+        return await api.canPlay(gameId, cardId)
       },
       changeWildCardColor: async (
         _: any,
         { gameId, chosenColor }: { gameId: number; chosenColor: string }
       ) => {
-        // return this.api.changeWildCardColor(gameId, chosenColor);
+        return await api.changeWildCardColor(gameId, chosenColor);
       },
     },
     Subscription: {
       pendingGamesFeed: {
-        // subscribe: () => this.pubsub.asyncIterableIterator(['pendingGamesFeed'])
+        subscribe: () => pubsub.asyncIterableIterator(['pendingGamesFeed'])
       },
 
       activeGamesFeed: {
-        // subscribe: () => this.pubsub.asyncIterableIterator(['activeGamesFeed'])
+        subscribe: () => pubsub.asyncIterableIterator(['activeGamesFeed'])
       }
     }
   }
-
-
-
-
-
 }
