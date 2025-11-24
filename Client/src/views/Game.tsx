866abf543ea770c084c4e53d5b5360c7649b64e7
KaBence <kabaly.bence@gmail.com>
2025-11-24 18:02:12 +0100

    statusbar fix

--- Client/src/views/Game.tsx
+++ Client/src/views/Game.tsx
@@ -116,7 +116,7 @@ const Game = () => {
     <div className='game-view'>
       <div className='game-board-area'>
         <div className='status-bar'>
-          <StatusBar message={statusMessage} isYourTurn={true} score={120} />
+          <StatusBar message={statusMessage} isYourTurn={isMyTurn}/>
         </div>
         <div className='player-bar'>
           <PlayersBar
